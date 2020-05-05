281aafb348d7d140fc65b26cdab1262c9876e6e0
Daniel Chavez <dchavez@guidehouse.com>
2020-05-05 17:42:06 -0400

    add contorller

---
+++ MultichainAPI/MultichainAPI/Data/MultichainRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using MultichainAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MultichainAPI.Data
+{
+    public class MultichainRepository : IMultichainRepository
+    {
+        private readonly IConfiguration _config;
+
+        public MultichainRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+        //public async Task<int> GetInfo()
+        //{
+        //    return Task<int>;
+        //}
+    }
+}
