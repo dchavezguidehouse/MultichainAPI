bb5a5ddea4e74ceee31a53fdf4d55679a905c134
Daniel Chavez <dchavez@guidehouse.com>
2020-05-05 13:43:11 -0400

    add new items

--- MultichainAPI/MultichainAPI/Models/WeatherForecast.cs
+++ MultichainAPI/MultichainAPI/Models/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace MultichainAPI
+namespace MultichainAPI.Models
 {
     public class WeatherForecast
     {
--- MultichainAPI/MultichainAPI/Startup.cs
+++ MultichainAPI/MultichainAPI/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MultichainAPI
 {
@@ -25,7 +19,21 @@ namespace MultichainAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddMemoryCache();
+
+            services.AddControllers()
+                .ConfigureApiBehaviorOptions(options =>
+                {
+                    options.SuppressConsumesConstraintForFormFileParameters = true;
+                    options.SuppressInferBindingSourcesForParameters = true;
+                    options.SuppressModelStateInvalidFilter = true;
+                });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v3", new OpenApiInfo { Title = "Multichain API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +45,17 @@ namespace MultichainAPI
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v3/swagger.json", "Multichain API v3");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
